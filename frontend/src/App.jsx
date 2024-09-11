7cb9d56b0e25a641d402a5b56671eba1dc8b263f
Krish <krishagarwal1673@gmail.com>
2024-09-11 12:29:36 +0530

    Add all the routes in the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import {BrowserRouter, Routes, Route } from 'react-router-dom'
+import {Signup} from "./pages/Signup"
+import {Signin} from "./pages/Signin"
+import {Dashboard} from "./pages/Dashboard"
+import {SendMoney} from "./pages/Transfer"
 
 function App() {
 
   return (
     <>
-    Hello
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/Signup" element = {<Signup />} />
+          <Route path = "/Signin" element = {<Signin />} />
+          <Route path = "/Dashboard" element = {<Dashboard />} />
+          <Route path = "/Transfer" element = {<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
