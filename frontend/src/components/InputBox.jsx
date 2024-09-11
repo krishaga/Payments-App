338d50a2d1eefea731003ff03e0bd706b4c9abd1
Krish <krishagarwal1673@gmail.com>
2024-09-11 13:29:57 +0530

    Refactor code for Input Box component and SignUp Page

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,9 +1,10 @@
 
 
 
-export function InputBox({label, placeholder}){
+export function InputBox({label, placeholder, isPassword=false}){
     return <div className="p-1">
         <div className="text-sm py-2 text-left font-medium pt-1 ">{label}</div>
-        <input placeholder={placeholder} className="border border-gray-200 rounded w-full px-2 py-1 "/>
+        
+        <input placeholder={placeholder} type={isPassword ? "password" : "text"}  className="border border-gray-200 rounded w-full px-2 py-1 "/>
     </div>
 }
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -14,7 +14,7 @@ export function Signup(){
                 <InputBox label={"First Name"} placeholder={"Enter First Name"}></InputBox>
                 <InputBox label={"Last Name"} placeholder={"Enter Last Name"}></InputBox>
                 <InputBox label={"Email"} placeholder={"Enter Email-id"}></InputBox>
-                <InputBox label={"Password"} placeholder={"Enter Password"}></InputBox>
+                <InputBox label={"Password"} isPassword={true} placeholder={"Enter Password"}></InputBox>
                 <button 
                     type="button" 
                     class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 my-4 w-full me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
