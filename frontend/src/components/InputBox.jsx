e9238ba7ab1565093e4a03ed40590045183c268c
Krish <krishagarwal1673@gmail.com>
2024-09-11 13:20:22 +0530

    Create Input Box component

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -2,8 +2,8 @@
 
 
 export function InputBox({label, placeholder}){
-    return <div className="">
+    return <div className="p-1">
         <div className="text-sm py-2 text-left font-medium pt-1 ">{label}</div>
-        <input placeholder={placeholder} className="border border-black rounded w-full px-2 py-1  "/>
+        <input placeholder={placeholder} className="border border-gray-200 rounded w-full px-2 py-1 "/>
     </div>
 }
