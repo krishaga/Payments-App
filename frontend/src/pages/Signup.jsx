c377258fdac37458cf5cb6c05a5c9186f0ab09b8
Krish <krishagarwal1673@gmail.com>
2024-09-12 11:21:13 +0530

    Refactor Code

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -2,10 +2,12 @@ import { Heading } from "../components/Heading";
 import {SubHeading} from "../components/SubHeading";
 import { InputBox } from "../components/InputBox";
 import { BottomWarning } from "../components/BottomWarning";
+import { useNavigate } from "react-router-dom";
 
 
 
 export function Signup(){
+    const navigate = useNavigate();
     return <div className=" bg-slate-400 h-screen flex justify-center ">
         <div className=" flex flex-col justify-center ">
             <div className=" bg-white w-80 p-4 pt-6 h-max rounded-lg text-center ">
@@ -17,8 +19,8 @@ export function Signup(){
                 <InputBox label={"Password"} isPassword={true} placeholder={"Enter Password"}></InputBox>
                 <button 
                     type="button" 
-                    class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 my-4 w-full me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-                    onClick={'/Dashboard'}>
+                    className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 my-4 w-full me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                    onClick={() => navigate('/Dashboard')}>
                     Sign up
                 </button>
                 <BottomWarning label={"Already have an account? "} buttonText={"Sign in"} to={"/Signin"}></BottomWarning>
