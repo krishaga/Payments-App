a77733c1992095546af254f0d482ee5e525d3471
Krish <krishagarwal1673@gmail.com>
2024-09-13 18:10:49 +0530

    Refactor Code of backend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,37 @@
 import { Navbar } from "../components/NavBar"
 import { Balance } from "../components/Balance"
 import { UsersComponent } from "../components/UsersComponent"
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
 export function Dashboard(){
+    const [balance,setBalance] = useState(0);
+
+    useEffect(() => {
+        const getBalance = async () => {
+            const data = await axios.get("http://localhost:3000/api/v1/account/balance",{
+                headers : {
+                    Authorization : "Bearer " + localStorage.getItem('token')
+                }
+            })
+            setBalance(data.data.amount)
+        }
+        getBalance()
+
+
+
+
+
+
+    },[])
+
+
+
     return <div>
-        <Navbar></Navbar>
+        <Navbar label={"Krish"}></Navbar>
         <div>
-            <Balance label={100000}></Balance>
+            <Balance label={balance}></Balance>
             <UsersComponent></UsersComponent>
         </div>
 
