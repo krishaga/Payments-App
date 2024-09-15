0dba4d967bdbd3b5963c3556839031bf9e9eda7d
Krish <krishagarwal1673@gmail.com>
2024-09-15 18:28:19 +0530

    Integrate Search Bar with diplay data to fetch search for profile based on user entry

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,8 +1,8 @@
 
 
 
-export function SearchBar(){
+export function SearchBar({onChange}){
     return <div className="mx-10">
-        <input type="search" name="" id="" placeholder="Search users...." className="pl-4 border border-gray-300 h-10 placeholder:text-gray-600 placeholder:font-medium rounded-md w-full" />
+        <input onChange={onChange}  placeholder="Search users...." className="pl-4 border border-gray-300 h-10 placeholder:text-gray-600 placeholder:font-medium rounded-md w-full" />
     </div>
 }
