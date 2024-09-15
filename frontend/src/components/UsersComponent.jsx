f1f1aac366e4bfa5be53eb5134cd9659a6678521
Krish <krishagarwal1673@gmail.com>
2024-09-15 18:45:06 +0530

    Fetch exisitng users data from backend and diplay it on screen

--- frontend/src/components/UsersComponent.jsx
+++ frontend/src/components/UsersComponent.jsx
@@ -1,33 +1,64 @@
+import { useEffect, useState } from "react";
 import { Heading } from "./Heading"
 import { SearchBar } from "./SearchBar"
 import {useNavigate} from "react-router-dom"
+import axios from "axios";
+
 
 export function UsersComponent({label}){ 
     const navigate = useNavigate();
+    const [search, setSearch] = useState("");
+    const [userData, setuserData] = useState([])
+
+    useEffect(() => {
+        const getData = async() => {
+            // const params = {
+            //     filter : {search}
+            // }
+            const response = await axios.get(`http://localhost:3000/api/v1/user/bulk?filter=${search}`,{
+                headers : {
+                    Authorization : "Bearer " + localStorage.getItem('token')
+                }
+                // params
+            })
+            setuserData(response.data.users)
+        }
+        getData()
+    },[search])
+
+
     return <div>
         <div className="font-bold text-2xl pl-10 pb-4 text-black">
             Users
         </div>
-        <SearchBar></SearchBar>
-        <div className="grid grid-cols-2 items-center m-10  ">
-            <div className="flex items-center justify-start">
-                <div className="w-9 h-9 rounded-full bg-slate-200 text-center flex items-center justify-center">
-                    U1
+        <SearchBar onChange = {e => {
+            setSearch(e.target.value);
+        }} ></SearchBar>
+
+        {
+            userData.map((user,index) =>
+            ( 
+                <div className="grid grid-cols-2 items-center m-10  ">
+                    <div className="flex items-center justify-start">
+                        <div className="w-9 h-9 rounded-full bg-slate-200 text-center flex items-center justify-center">
+                            U{index+1}
+                        </div>
+                        <div className="font-bold text-lg  pl-5  ">
+                            {user.firstName} {user.lastName}
+                        </div>
+                    </div>
+                    <div className="flex justify-end -mr-2">
+                        <button 
+                            type="button" 
+                            className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm p-4 px-6 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                            onClick =  { () =>
+                                navigate('/Transfer')}>
+                            Send Money
+                        </button>  
+                    </div>
                 </div>
-                <div className="font-bold text-lg  pl-5  ">
-                    User 1
-                </div>
-            </div>
-            <div className="flex justify-end -mr-2">
-                <button 
-                    type="button" 
-                    className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm p-4 px-6 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
-                    onClick =  { () =>
-                        navigate('/Transfer')}>
-                    Send Money
-                </button>
-                
-            </div>
-        </div>
+                )      
+        )}
+
     </div>
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -7,6 +7,7 @@ import axios from "axios";
 
 export function Dashboard(){
     const [balance,setBalance] = useState(0);
+    const [label, setUserName] = useState("");
 
     useEffect(() => {
         const getBalance = async () => {
@@ -18,18 +19,26 @@ export function Dashboard(){
             setBalance(data.data.amount)
         }
         getBalance()
+    },[])
 
-
-
-
-
-
+    useEffect(() => {
+        const gettingData = async() => {
+        try{
+            const data = await axios.get('http://localhost:3000/api/v1/user/userdetailbyid',{
+                headers : {
+                    Authorization : "Bearer " + localStorage.getItem('token')
+                }
+            })
+            setUserName(data.data.userdetails[0].firstName)
+        }catch(err){
+            window.alert(data.data.message)
+        }}
+        gettingData()
     },[])
 
 
-
     return <div>
-        <Navbar label={"Krish"}></Navbar>
+        <Navbar label={label}></Navbar>
         <div>
             <Balance label={balance}></Balance>
             <UsersComponent></UsersComponent>
